e4dc729506bd4482db9ddde2804601a4b71a7b9a
yinbp <yin-bp@163.com>
2016-11-12 22:17:49 +0800

    将令牌管理模块从bboss-security模块中剥离

--- bboss-security/src/org/frameworkset/web/token/SecurityMonitorServlet.java
+++ bboss-security/src/org/frameworkset/web/token/SecurityMonitorServlet.java
@@ -1,5 +1,7 @@
 package org.frameworkset.web.token;
 
+import java.lang.reflect.Method;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,13 +16,28 @@ import org.frameworkset.security.session.SessionUtil;
  */
 public class SecurityMonitorServlet extends HttpServlet {
 	private static Logger log = Logger.getLogger(SecurityMonitorServlet.class);
+	private static Method getTokenService;
+	static 
+	{
+		try {
+			Class clazz = Class.forName("org.frameworkset.web.token.TokenHelper");
+			getTokenService = clazz.getMethod("getTokenService");
+		} catch (ClassNotFoundException e) {
+			 
+		} catch (NoSuchMethodException e) {
+			 
+		} catch (SecurityException e) {
+			 
+		}
+	}
+	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		
 		super.init(config);
 		try {
 			
-			 TokenHelper.getTokenService();
+			getTokenService.invoke(null);
 			
 		} catch (Throwable e) {
 			log.warn("",e);
